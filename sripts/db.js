cbc156aaf00d97b404d1baeb0f14db95205e86cb
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-01-22 23:22:43 +0100

    add db name, price and description

---
+++ sripts/db.js
@@ -0,0 +1,62 @@
+let myDishes = [
+    {
+        "name": "Veggie mushroom black burger",
+        "price": 16.90,
+        "description:": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    },
+    {
+        "name": "All meat burger",
+        "price": 15.90,
+        "description:":"Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+    },
+     {
+        "name": "Beef red burger",
+        "price" : 14.90,
+        "description:": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    },
+     {
+        "name": "Big chicken burger",
+        "price": 15.90,
+        "description:":"Beef, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+    },
+    {
+        "name": "Pizza Margherita",
+        "price": 11.90,
+        "description": "Tomato Sauce, Mozzarella"
+    },
+    {
+        "name": "Pizza Chorizo",
+        "price": 13.90,
+        "description": "Tomato slices, Mozarella, Chorizo"
+    },
+    {
+        "name": "Funghi",
+        "price": 12.90,
+        "description": "Red onion, Olives, Button Mushrooms, Mozarella"
+    },
+    {
+        "name": "Quattro Formaggi with Chichen",
+        "price": 15.90,
+        "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+    },
+    {
+        "name": "Warm beef arugula salad",
+        "price": 16.90,
+        "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+    },
+    {
+        "name": "Mini green Salad",
+        "price": 7.90,
+        "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+    },
+    {
+        "name": "Green Salad with sea food",
+        "price": 16.90,
+        "description": "Mixed greens, Cherry tomatoe, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    },
+    {
+        "name": "Vegan green salad with tofu",
+        "price": 14.90,
+        "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+    },
+]
