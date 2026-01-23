2fc22465ea8143d2b33dd8ea7f6d4596ff3d2d55
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-01-23 19:46:43 +0100

    changes

--- script.js
+++ script.js
@@ -3,17 +3,7 @@ let menuListContainer = document.getElementById('menu-list');
 function generateMenu(){
     document.getElementById("menu_container"). innerHTML = "";
     for (let i = 0; i < myDishes.length; i++) {
-        
-        
     document.getElementById("menu_container").innerHTML += generateMenuHtml(i);
     }
 }
-
-// myDishes.forEach((myDishes,i) => {
-//     menuListContainer.innerHTML += `<li>${myDishes}</li>`;
-//     if((i +1) % 4 === 0) {
-//         menuListContainer.innerHTML += `<h3>Neuer Kategorie</h3>`
-//     }
-//     });
-
 
--- sripts/templates.js
+++ sripts/templates.js
@@ -2,13 +2,14 @@ function generateMenuHtml(i){
     return`
     <div class="style_menu_box">
 
-    <img class="" src="./assets/img/${myDishes[i].img}"  alt="${myDishes[i].alt}"
+    <img src="./assets/img/${myDishes[i].img}"  alt="${myDishes[i].alt}"
     
     <div>
    <div> <h3>${myDishes[i].name}</h3>
     <span> ${myDishes[i].description}</span> </div>
 
-    <div class="prices"> <span> ${myDishes[i].price.toFixed(2)} </span> </div>
+    <div class="prices"> <span> ${myDishes[i].price.toFixed(2)}€ </span> 
+    <img  src="./assets/fonts/${myDishes[i].btn}"  alt="${myDishes[i].btnalt}"></div>
     
     
     
