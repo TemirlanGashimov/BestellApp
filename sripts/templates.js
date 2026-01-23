cc4bb2308d1208e699976f09700198ed1426b931
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-01-23 01:15:49 +0100

    add src and alt

--- sripts/templates.js
+++ sripts/templates.js
@@ -3,6 +3,8 @@ function generateMenuHtml(i){
     <div class="">
     <h3>${myDishes[i].name}</h3>
     
+    <img class="" src="./assets/img/${myDishes[i].img}"  alt="${myDishes[i].alt}"</div>
+
     <p> ${myDishes[i].price.toFixed(2)} </p>
     
     <p> ${myDishes[i].description}</p>
