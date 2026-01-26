3df07eaf53080d1c3287988cd8328ddc219196e8
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-01-26 17:02:55 +0100

    added new function

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let menuListContainer = document.getElementById('menu-list');
+let basketRef =document.getElementById("basket");
 
 function generateMenu(){
     document.getElementById("menu_container"). innerHTML = "";
     for (let i = 0; i < myDishes.length; i++) {
     document.getElementById("menu_container").innerHTML += generateMenuHtml(i);
     }
+    
+  
 }
 
+
+function addToBasket(i){
+myDishes[i].amount++
+
+}
