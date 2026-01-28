dd151afbc2825bced12968e1a24e6763ae4619bf
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-01-28 17:27:08 +0100

    changes

--- scripts/templates.js
+++ scripts/templates.js
@@ -4,11 +4,11 @@ function generateMenuHtml(i) {
 
     <img src="./assets/img/${myDishes[i].img}"  alt="${myDishes[i].alt}">
     
-    <div>
+    <div class="menu_style">
    <div> <h3>${myDishes[i].name}</h3>
     <span> ${myDishes[i].description}</span> </div>
 
-    <div class="prices"> <span> ${myDishes[i].price.toFixed(2)}€ </span> 
+    <div class= "prices"> <span> ${myDishes[i].price.toFixed(2)}€ </span> 
     <img  src="./assets/fonts/${myDishes[i].btn}"  alt="${myDishes[i].btnalt}" onclick="addToBasket(${i})"></div>
     </div>
     `;
